b1a17faefe76136d7f49de16bc3bf1900bf38942
MMercedesCantarero <mcantarero@frba.utn.edu.ar>
2024-05-08 23:37:59 -0300

    Punto 1, modelado de ciudades y creacion de funcion Valor de una ciudad

---
+++ haskell/src/Funcity.hs
@@ -0,0 +1,24 @@
+import Text.Show.Functions ()
+
+--Punto 1 
+data Ciudad = UnaCiudad {
+  nombre         :: String,
+  fechaFundacion :: Int,
+  atracciones    :: [String],
+  costoDeVida    :: Int
+} deriving Show
+
+baradero :: Ciudad
+baradero = UnaCiudad "Baradero" 1615 ["Parque del Este", "Museo Alejandro Barbich"] 150 
+
+nullish :: Ciudad
+nullish = UnaCiudad "Nullish" 1800 [] 140
+
+caletaOlivia :: Ciudad
+caletaOlivia = UnaCiudad "Caleta Olivia" 1901 ["El Gorosito", "Faro Costanera"] 120
+
+valorDeUnaCiudad :: Ciudad -> Int
+valorDeUnaCiudad unaCiudad
+    | fechaFundacion unaCiudad <= 1800 = (1800 - fechaFundacion unaCiudad) * 5
+    | null (atracciones unaCiudad)     = costoDeVida unaCiudad * 2
+    | otherwise                        = costoDeVida unaCiudad * 3

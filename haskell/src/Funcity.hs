c1a1f33c2dedb14a00ae6aff152e73c850b7a8ab
Tomas Santolaria <tomassantolaria@Air-de-Tomas.fibertel.com.ar>
2024-05-12 21:39:06 -0300

    agregue la funcion tieneNombreRaro

--- haskell/src/Funcity.hs
+++ haskell/src/Funcity.hs
@@ -17,8 +17,18 @@ nullish = UnaCiudad "Nullish" 1800 [] 140
 caletaOlivia :: Ciudad
 caletaOlivia = UnaCiudad "Caleta Olivia" 1901 ["El Gorosito", "Faro Costanera"] 120
 
-valorDeUnaCiudad :: Ciudad -> Int
+valorDeUnaCiudad :: Ciudad -> Float
 valorDeUnaCiudad unaCiudad
     | fechaFundacion unaCiudad <= 1800 = (1800 - fechaFundacion unaCiudad) * 5
     | null (atracciones unaCiudad)     = costoDeVida unaCiudad * 2
     | otherwise                        = costoDeVida unaCiudad * 3
+
+
+tieneNombreRaro :: Ciudad -> Bool
+tieneNombreRaro = (<5) . length . nombre
+
+maipu :: Ciudad
+maipu = UnaCiudad "Maipu" 1878 ["Fortin Kakel"] 115
+
+azul :: Ciudad
+azul = UnaCiudad "Azul" 1832 ["Teatro Español", "Parque Municipal Sarmiento", "Costanera Cacique Catriel"] 190

7f5da5c00609cbf7ae810491138119de37c6b045
MMercedesCantarero <mcantarero@frba.utn.edu.ar>
2024-07-25 23:53:58 -0300

    base de conocimiento, punto 1

---
+++ prolog/potencialRebelde.pl
@@ -0,0 +1,21 @@
+%punto1
+% persona(Nombre, Profesion, Gusto, Habilidad, HistorialCriminal)
+
+persona(bakunin, aviacionMilitar, noTiene, manejandoAutos, roboDeAeronaves).
+persona(bakunin, aviacionMilitar, noTiene, manejandoAutos, fraude).
+persona(bakunin, aviacionMilitar, noTiene, manejandoAutos, tenenciaDeCafeina).
+persona(ravachol, inteligenciaMilitar, juegosDeAzar, tiroAlBlanco, falsificacionDeVacunas).
+persona(ravachol, inteligenciaMilitar, ajedrez, tiroAlBlanco, fraude).
+persona(ravachol, inteligenciaMilitar, tiroAlBlanco, tiroAlBlanco, fraude).
+persona(rosaDubovsky, recolectorDeBasura, construirPuentes, construyendoPuentes, noTiene).
+persona(rosaDubovsky, asesinaASueldo, mirarPeppaPig, mirandoPeppaPig, noTiene).
+persona(rosaDubovsky, asesinaASueldo, fisicaCuantica, mirandoPeppaPig, noTiene).
+persona(emmaGoldman, profesoraDeJudo, judo, judo, noTiene).
+persona(emmaGoldman, cineasta, carrerasdeAutomovilismo, armandoBombas, noTiene).
+persona(judithButler, profesoraDeJudo, judo, judo, falsificacionDeCheques).
+persona(judithButler, inteligenciaMilitar, carrerasdeAutomovilismo, judo, fraude).
+persona(elisaBachofen, ingenieriaMecanica, fuego, armandoBombas, noTiene).
+persona(elisaBachofen, ingenieriaMecanica, destruccion, armandoBombas, noTiene).
+persona(juanSuariano, noSeSabe, judo, judo, falsificacionDeDinero).
+persona(juanSuariano, noSeSabe, armarBombas, armandoBombas, fraude).
+persona(juanSuariano, noSeSabe, ringraje, ringraje, fraude).

260240a6d622a65376dbdc382ed4ca1daa908435
Mora <mocontreras@frba.utn.edu.ar>
2024-07-28 16:25:36 -0300

    punto 2 y punto 5

--- prolog/potencialRebelde.pl
+++ prolog/potencialRebelde.pl
@@ -38,3 +38,46 @@ historialCriminal([roboAeronaves,fraude,tenenciaCafeina],bakunin).
 historialCriminal([falsificacionVacunas,fraude],ravacol).
 historialCriminal([falsificacionCheques,fraude],judithButler).
 historialCriminal([falsificacionDinero,fraude],juanSuriano).
+
+% PUNTO 2
+tieneVivienda([bakunin,elisaBachofen,rosaDubovsky],laSaverino).
+tieneVivienda([ravachol],comisaria48).
+tieneVivienda([emmaGoldman,juanSuriano,judithButler],laCasaDePapel).
+
+tieneEsconditesVivienda(laSaverino,[cuatoSecreto(4,8),pasadizos(1),tunel(8,construido),tunel(5,construido),tunel(1,enConstruccion)]).
+tieneEsconditesVivienda(laCasaDePapel,[pasadizos(2),cuartoSecreto(5,3),cuartoSecreto(4,7),tunel(9,construido),tunel(2,construido)]).
+tieneEsconditesVivienda(casaDelSolNaciente,[pasadizos(1),tunel(3,sinConstruir)]).
+
+% PUNTO 5
+
+esActividadTerrorista(Actividad):-
+    member(Actividad,[armarBombas,tiroAlBlanco,mirarPeppaPig]).
+
+noTieneGustosOSoloTieneBuenos(Alguien):-
+    not(tieneGustos(Alguien,_)).
+
+noTieneGustosOSoloTieneBuenos(Alguien):-
+    soloTieneGustosBuenos(Alguien).
+
+soloTieneGustosBuenos(Alguien):-
+    tieneGustos(Alguien,Gustos),
+    forall(member(Gusto,Gustos), not(esActividadTerrorista(Gusto))).
+    
+tieneHabilidadesTerroristas(Alguien):-
+    habilidadesDe(Alguien,Habilidades),
+    member(Habilidad,Habilidades),
+    esActividadTerrorista(Habilidad).
+
+convivientesDe(Persona, Convivientes) :-
+    tieneVivienda(Convivientes, Vivienda),
+    member(Persona, Convivientes).
+
+largoHistorialCriminalEnVivienda(Alguien):-
+    convivientesDe(Alguien,Convivientes),
+    member(unConviviente,Convivientes),
+    tieneLargoHistorialCriminal(unConviviente).
+
+tieneLargoHistorialCriminal(Alguien):-
+    historialCriminal(Historial,Alguien),
+    length(Historial,Largo),
+    Largo > 1.
